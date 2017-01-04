d2d254802834f2dd96cda3c04fc68e9e6d0ee872
Gnomeus <agomezgu@gmail.com>
2017-01-04 17:14:23 -0500

    Added Delegates , Actions , Functions<T,TResult> and Expressions Lambda Project

---
+++ BasicConcepts/Examples.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace BasicConcepts
+{
+    partial class Examples
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_DelegateExamples = new System.Windows.Forms.Button();
+            this.Btn_ActionsExample = new System.Windows.Forms.Button();
+            this.Btn_FuncExample = new System.Windows.Forms.Button();
+            this.Btn_LambdaExample = new System.Windows.Forms.Button();
+            this.LinkLbl_SeeDocumentation = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_DelegateExamples
+            // 
+            this.Btn_DelegateExamples.Location = new System.Drawing.Point(93, 12);
+            this.Btn_DelegateExamples.Name = "Btn_DelegateExamples";
+            this.Btn_DelegateExamples.Size = new System.Drawing.Size(75, 23);
+            this.Btn_DelegateExamples.TabIndex = 0;
+            this.Btn_DelegateExamples.Text = "Delegates!!";
+            this.Btn_DelegateExamples.UseVisualStyleBackColor = true;
+            this.Btn_DelegateExamples.Click += new System.EventHandler(this.Btn_DelegateExamples_Click);
+            // 
+            // Btn_ActionsExample
+            // 
+            this.Btn_ActionsExample.Location = new System.Drawing.Point(93, 57);
+            this.Btn_ActionsExample.Name = "Btn_ActionsExample";
+            this.Btn_ActionsExample.Size = new System.Drawing.Size(75, 25);
+            this.Btn_ActionsExample.TabIndex = 1;
+            this.Btn_ActionsExample.Text = "Actions !!";
+            this.Btn_ActionsExample.UseVisualStyleBackColor = true;
+            this.Btn_ActionsExample.Click += new System.EventHandler(this.Btn_ActionsExample_Click);
+            // 
+            // Btn_FuncExample
+            // 
+            this.Btn_FuncExample.Location = new System.Drawing.Point(53, 107);
+            this.Btn_FuncExample.Name = "Btn_FuncExample";
+            this.Btn_FuncExample.Size = new System.Drawing.Size(158, 25);
+            this.Btn_FuncExample.TabIndex = 2;
+            this.Btn_FuncExample.Text = "Functions <T , TResult>";
+            this.Btn_FuncExample.UseVisualStyleBackColor = true;
+            this.Btn_FuncExample.Click += new System.EventHandler(this.Btn_FuncExample_Click);
+            // 
+            // Btn_LambdaExample
+            // 
+            this.Btn_LambdaExample.Location = new System.Drawing.Point(93, 157);
+            this.Btn_LambdaExample.Name = "Btn_LambdaExample";
+            this.Btn_LambdaExample.Size = new System.Drawing.Size(75, 25);
+            this.Btn_LambdaExample.TabIndex = 3;
+            this.Btn_LambdaExample.Text = "Lambdas !!";
+            this.Btn_LambdaExample.UseVisualStyleBackColor = true;
+            this.Btn_LambdaExample.Click += new System.EventHandler(this.Btn_LambdaExample_Click);
+            // 
+            // LinkLbl_SeeDocumentation
+            // 
+            this.LinkLbl_SeeDocumentation.AutoSize = true;
+            this.LinkLbl_SeeDocumentation.Location = new System.Drawing.Point(12, 199);
+            this.LinkLbl_SeeDocumentation.Name = "LinkLbl_SeeDocumentation";
+            this.LinkLbl_SeeDocumentation.Size = new System.Drawing.Size(176, 13);
+            this.LinkLbl_SeeDocumentation.TabIndex = 9;
+            this.LinkLbl_SeeDocumentation.TabStop = true;
+            this.LinkLbl_SeeDocumentation.Text = "See Online Project Documentation!!";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(122, 240);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Developed by Gnomeus";
+            // 
+            // Examples
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 261);
+            this.Controls.Add(this.LinkLbl_SeeDocumentation);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Btn_LambdaExample);
+            this.Controls.Add(this.Btn_FuncExample);
+            this.Controls.Add(this.Btn_ActionsExample);
+            this.Controls.Add(this.Btn_DelegateExamples);
+            this.Name = "Examples";
+            this.Text = "From Delegate to Lambdas  :-)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_DelegateExamples;
+        private System.Windows.Forms.Button Btn_ActionsExample;
+        private System.Windows.Forms.Button Btn_FuncExample;
+        private System.Windows.Forms.Button Btn_LambdaExample;
+        private System.Windows.Forms.LinkLabel LinkLbl_SeeDocumentation;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BasicConcepts/Examples.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicConcepts
+{
+    public partial class Examples : Form
+    {
+        private delegate void DoOperation(string par);
+        private delegate string DoOperationWithReturn(string par);
+        private delegate string DoOperationWithReturnAndTwoParameters(string par1 , string par2);
+
+        public Examples()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_DelegateExamples_Click(object sender, EventArgs e)
+        {
+            //Instantiate delegate with named Methods
+            DoOperation op = NamedVoidMethod;
+            DoOperationWithReturn opReturn = NamedMethodWithStringReturn;
+
+            //Invoke Delegate methods
+            op("Required Par");
+            var returnedValueFromDelegate = opReturn("Required Par");
+            Console.WriteLine(returnedValueFromDelegate);
+
+            //Instantiate with Anonymus method
+            op = delegate(string par)
+            {
+                Console.WriteLine(par);
+            };
+            op("3-You wrote this in anonymus method oh yeahh!!!!");
+
+
+            // Finally ..... Instantiate with lambda Instrucction
+            // hey hey it's diferent to Lambda expression
+
+            opReturn = par =>
+            {
+                Console.WriteLine("4-Call Lambda Instruction!!!!");
+                return "Returned from delegate Method with lambda Instruction }:) ";
+            };
+            var returnedValueFromDelegateLambdaInst = opReturn("Execute Lambda String Method!!!!");
+            Console.WriteLine(returnedValueFromDelegateLambdaInst);
+
+        }
+
+        private void NamedVoidMethod(string par)
+        {
+            Console.WriteLine("1- Call Void Method!!!!");
+        }
+
+        private string NamedMethodWithStringReturn(string par)
+        {
+            Console.WriteLine("2- Call String Method!!!!");
+            return "Returned from delegate Method :) :-]...";
+        }
+
+        private void Btn_ActionsExample_Click(object sender, EventArgs e)
+        {
+            //Ptssss Ptssss this are Lambda Expression ... 
+            Action<string> action = x=>MessageBox.Show(x);
+            Action<string> action2 =y => Console.WriteLine(y);
+
+            INeedActionToExecute(action,"Action 1 Executed");
+            INeedActionToExecute(action2, "Action 2 Executed");
+        }
+
+        private void INeedActionToExecute(Action<string> actionToExecute  , string valueToWrite)
+        {
+            actionToExecute(valueToWrite);
+        }
+
+        private void Btn_FuncExample_Click(object sender, EventArgs e)
+        {
+            //Ptssss Ptssss this are Lambda Instruction ... 
+            Func<int, string> func = (x) =>
+            {
+                MessageBox.Show(x.ToString());
+                return "I show a Message Box --- Parameter Received is :"+x.ToString(); 
+            };
+            Func<int, string> func2 = (y) =>
+            {
+                Console.WriteLine(y.ToString());
+                return "I write in Console --- Parameter Received is :" + y.ToString();
+            };
+
+            Console.WriteLine(INeedFunctionToExecute(func, 1000));
+            Console.WriteLine(INeedFunctionToExecute(func2, 2000));
+        }
+
+        private string INeedFunctionToExecute(Func<int , string> functionToExecute, int inParameter)
+        {
+            return functionToExecute(inParameter);
+        }
+
+        private void Btn_LambdaExample_Click(object sender, EventArgs e)
+        {
+            const string msg = "Lambda Example ";
+
+            DoOperation lambdaVoid = par => Console.WriteLine(par);
+
+            DoOperationWithReturn lambdaReturnStringInstruction = par =>
+            {
+                return par;
+            };
+            DoOperationWithReturn lambdaReturnStringExpression = par => par;
+
+            DoOperationWithReturnAndTwoParameters lambdaReturnStringInstructionTwoParameters = (par1, par2) =>
+            {
+                return par1 + par2;
+            };
+
+            lambdaVoid(msg+1);
+           Console.WriteLine(lambdaReturnStringInstruction(msg + 2));
+            Console.WriteLine(lambdaReturnStringExpression(msg + 3));
+            Console.WriteLine(lambdaReturnStringInstructionTwoParameters(msg , 4.ToString()));
+
+        }
+    }
+}
